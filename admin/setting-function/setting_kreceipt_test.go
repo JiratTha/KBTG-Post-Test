535226334bbdb7bf6a9022e31248805d2c95422d
JiratTha <jirat.thammanan@gmail.com>
2024-04-28 17:30:45 +0700

    refactor

--- admin/setting-function/setting_kreceipt_test.go
+++ admin/setting-function/setting_kreceipt_test.go
@@ -13,25 +13,21 @@ import (
 )
 
 func TestSettingKReceipt(t *testing.T) {
-	// Create a new mock database connection
+
 	dbMock, mock, err := sqlmock.New()
 	if err != nil {
 		log.Fatalf("An error '%s' was not expected when opening a stub database connection", err)
 	}
 	defer dbMock.Close()
 
-	// Wrap the *sql.DB from sqlmock into an *sqlx.DB
 	sqlxDB := sqlx.NewDb(dbMock, "sqlmock")
 
-	// Inject the mock database into your singleton
 	db.SetDB(sqlxDB)
 
-	// Define expectations for SQL calls
 	mock.ExpectExec(`UPDATE project1."allowance" SET amount=\$1 WHERE allowance_type='k-receipt'`).
 		WithArgs(sqlmock.AnyArg()).
 		WillReturnResult(sqlmock.NewResult(1, 1)) // Mock successful update
 
-	// Test when Amount is greater than 100000
 	t.Run("AmountGreaterThan100000", func(t *testing.T) {
 		admin := model.Admin{Amount: 150000}
 		expected := model.AdminResponse{KReceipt: 100000}
@@ -40,7 +36,6 @@ func TestSettingKReceipt(t *testing.T) {
 		assert.Equal(t, expected, result, "Expected and actual results should match")
 	})
 
-	// Test when Amount is less than 100000
 	t.Run("AmountLessThan100000", func(t *testing.T) {
 		admin := model.Admin{Amount: 80000}
 		expected := model.AdminResponse{KReceipt: 80000}
@@ -49,7 +44,6 @@ func TestSettingKReceipt(t *testing.T) {
 		assert.Equal(t, expected, result, "Expected and actual results should match")
 	})
 
-	// Check if all expectations were met
 	if err := mock.ExpectationsWereMet(); err != nil {
 		t.Errorf("there were unfulfilled expectations: %s", err)
 	}
