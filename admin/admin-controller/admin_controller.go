8a7da50753e7eb8f8af5ef9ec4a11af27f66952e
JiratTha <jirat.thammanan@gmail.com>
2024-04-28 17:26:19 +0700

    change return status to BadRequest

--- admin/admin-controller/admin_controller.go
+++ admin/admin-controller/admin_controller.go
@@ -31,7 +31,7 @@ func SetPersonalDeductPost(c echo.Context) error {
 	if reqPersonalAmount.Amount <= 10000 {
 		return echo.NewHTTPError(http.StatusBadRequest, "Personal deduction must be greater than 10000")
 	} else if reqPersonalAmount.Amount > 100000 {
-		return echo.NewHTTPError(http.StatusBadRequest, "Personal deduction must be less than 100000")
+		return echo.NewHTTPError(http.StatusBadRequest, "Personal deduction must be less than or equal 100000")
 	}
 	newPersonalDeduct := setting.SettingPersonalDeduction(reqPersonalAmount)
 	return c.JSON(http.StatusOK, newPersonalDeduct)
@@ -60,7 +60,7 @@ func SetKReceiptPost(c echo.Context) error {
 	if reqKReceiptAmount.Amount <= 0 {
 		return echo.NewHTTPError(http.StatusBadRequest, "K-receipt must be greater than 0")
 	} else if reqKReceiptAmount.Amount > 100000 {
-		return echo.NewHTTPError(http.StatusBadRequest, "K-receipt must be less than 100000")
+		return echo.NewHTTPError(http.StatusBadRequest, "K-receipt must be less than or equal 100000")
 	}
 	newKReceipt := setting.SettingKReceipt(reqKReceiptAmount)
 	return c.JSON(http.StatusOK, newKReceipt)
